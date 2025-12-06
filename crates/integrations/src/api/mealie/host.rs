76482c508312062a0933ef3180a385cf0d910a80
reaper47 <macpoule@gmail.com>
2025-12-06 09:03:09 -0500

    Fix tests

--- crates/integrations/src/api/mealie/host.rs
+++ crates/integrations/src/api/mealie/host.rs
@@ -39,3 +39,86 @@ impl Host {
         format!("{}/api/admin/users/{user_id}", self.0)
     }
 }
+
+impl AsRef<str> for Host {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use uuid::Uuid;
+
+    use super::*;
+
+    #[test]
+    fn test_host_new() {
+        let host = Host::new("https://example.com");
+
+        assert_eq!(host.as_ref(), "https://example.com");
+    }
+
+    #[test]
+    fn test_host_login_url() {
+        let got = Host::new("https://example.com").login_url();
+
+        assert_eq!(got, "https://example.com/api/auth/token");
+    }
+
+    #[test]
+    fn test_host_logout_url() {
+        let got = Host::new("https://example.com").logout_url();
+
+        assert_eq!(got, "https://example.com/api/auth/logout");
+    }
+
+    #[test]
+    fn test_host_recipe_image_url() {
+        let host = Host::new("https://example.com");
+        let recipe_id = Uuid::new_v4();
+
+        let got = host.recipe_image_url(recipe_id);
+
+        assert_eq!(
+            got,
+            format!(
+                "https://example.com/api/media/recipes/{}/images/original.webp",
+                recipe_id
+            )
+        );
+    }
+
+    #[test]
+    fn test_host_recipe_url() {
+        let host = Host::new("https://example.com");
+        let recipe_id = Uuid::new_v4();
+
+        let got = host.recipe_url(recipe_id);
+
+        assert_eq!(
+            got,
+            format!("https://example.com/api/recipes/{}", recipe_id)
+        );
+    }
+
+    #[test]
+    fn test_host_recipes_url() {
+        let got = Host::new("https://example.com").recipes_url("1".to_string());
+
+        assert_eq!(got, "https://example.com/api/recipes?page=1");
+    }
+
+    #[test]
+    fn test_host_user_url() {
+        let host = Host::new("https://example.com");
+        let user_id = Uuid::new_v4();
+
+        let got = host.user_url(user_id);
+
+        assert_eq!(
+            got,
+            format!("https://example.com/api/admin/users/{}", user_id)
+        );
+    }
+}
--- crates/integrations/src/api/mod.rs
+++ crates/integrations/src/api/mod.rs
@@ -3,9 +3,11 @@ mod common;
 pub mod mealie;
 
 pub use common::{Credentials, FailedRecipes};
+use futures::{StreamExt, pin_mut};
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashMap, str::FromStr, sync::Arc};
 
 use async_stream::stream;
 use futures_core::Stream;
@@ -60,9 +62,8 @@ impl Api {
         stream! {
             match self {
                 Api::Mealie => {
-
                         let mealie = match Mealie::new(MealieRecipeClient::new(host)).login(credentials).await {
-                            Ok(m) => m,
+                            Ok(m) => Arc::new(m),
                             Err(err) => {
                                 yield Err((Uuid::nil(), 0,  err));
                                 return;
@@ -78,18 +79,37 @@ impl Api {
                         };
 
                         let num_recipes = recipe_ids.len() as i64;
-                        let mut users = HashMap::<Uuid, MealieUser>::new();
 
-                        for id in recipe_ids {
-                            match mealie.fetch_recipe(id, &mut users).await {
-                                Ok(recipe) => yield Ok((id, recipe, num_recipes)),
-                                Err(err) => yield Err((err.0, num_recipes, err.1)),
+                        {
+                            let users = Arc::new(Mutex::new(HashMap::<Uuid, MealieUser>::new()));
+
+                            let fetches = futures::stream::iter(recipe_ids).map(|id| {
+                                let users = Arc::clone(&users);
+                                let mealie = Arc::clone(&mealie);
+
+                                async move {
+                                    let mut users_guard = users.lock().await;
+                                    let recipe = mealie.fetch_recipe(id, &mut users_guard).await;
+                                    (id, recipe)
+                                }
+                            }).buffer_unordered(10);
+
+                            pin_mut!(fetches);
+                            while let Some((id, recipe)) = fetches.next().await {
+                                match recipe {
+                                    Ok(recipe) => yield Ok((id, recipe, num_recipes)),
+                                    Err(err) => yield Err((err.0, num_recipes, err.1)),
+                                }
                             }
                         }
 
-                        if let Err(err) = mealie.logout().await {
-                            yield Err((Uuid::nil(), num_recipes, err));
-                            return;
+                        match Arc::try_unwrap(mealie) {
+                            Ok(mealie) => {
+                                if let Err(err) = mealie.logout().await {
+                                    yield Err((Uuid::nil(), num_recipes, err));
+                                }
+                            },
+                            Err(_) => yield Err((Uuid::nil(), num_recipes, Error::ApiError("Failed to logout: Arc still shared".into()))),
                         }
                 }
                 Api::Nextcloud => todo!("Nextcloud API not implemented"),
