c5a23b31af2b2b842cc4d029556c8aaa3f2879c8
reaper47 <macpoule@gmail.com>
2026-03-20 21:18:38 -0400

    Fix tests

--- crates/router/src/recipes_router/tests/supported_websites.rs
+++ crates/router/src/recipes_router/tests/supported_websites.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 mod tests {
     use axum::http::Method;
-    use axum_test::http::header::CONTENT_TYPE;
+    use axum_test::{TestResponse, http::header::CONTENT_TYPE};
 
+    use recipya_scraper::Website;
     use testing::utils::{TestDb, assert_must_be_logged_in, build_server_logged_in};
 
     type Result<T> = core::result::Result<T, Box<dyn std::error::Error>>;
@@ -19,11 +20,13 @@ mod tests {
         let (_test_db, config) = TestDb::new(None).await?;
         let server = build_server_logged_in(config).await?;
 
-        let res = server.get(BASE_URI).await;
+        let res: TestResponse = server.get(BASE_URI).await;
 
         res.assert_status_ok();
         res.assert_header(CONTENT_TYPE, "text/html; charset=utf-8");
-        res.assert_text_contains(r#"<tr class="text-center"><td>1</td><td><a class="underline" href="https://https://www.allrecipes.com/" target="_blank">Allrecipes</a></td></tr><tr class="text-center"><td>2</td><td><a class="underline" href="https://https://zaatarandzaytoun.com/recipe-index/" target="_blank">Zaatar & Zaytoun</a></td></tr><tr class="text-center"><td>3</td><td><a class="underline" href="https://https://zabihahalal.com/recipes/" target="_blank">Zabiha Halal</a></td></tr><tr class="text-center"><td>4</td><td><a class="underline" href="https://https://zagleft.com/recipe-list/" target="_blank">Zagleft</a></td></tr><tr class="text-center"><td>5</td><td><a class="underline" href="https://https://zardyplants.com" target="_blank">ZardyPlants</a></td></tr><tr class="text-center"><td>6</td><td><a class="underline" href="https://https://zarskitchen.com" target="_blank">Zars Kitchen</a></td></tr><tr class="text-center"><td>7</td><td><a class="underline" href="https://https://zeezest.com/recipes/" target="_blank">ZZest</a></td></tr><tr class="text-center"><td>8</td><td><a class="underline" href="https://https://www.zenbelly.com/recipes/" target="_blank">Zen Belly</a></td></tr><tr class="text-center"><td>9</td><td><a class="underline" href="https://https://zenhealth.net/recipe-index/" target="_blank">Zenhealth</a></td></tr><tr class="text-center"><td>10</td><td><a class="underline" href="https://https://zenkimchi.com" target="_blank">Zen Kimchi</a></td></tr><tr class="text-center"><td>11</td><td><a class="underline" href="https://https://zestfulkitchen.com/category/all-recipes-from-zestful-kitchen/" target="_blank">Zestful Kitchen</a></td></tr><tr class="text-center"><td>12</td><td><a class="underline" href="https://https://zestysouthindiankitchen.com" target="_blank">Zesty South Indian Kitchen</a></td></tr><tr class="text-center"><td>13</td><td><a class="underline" href="https://https://zhangcatherine.com" target="_blank">Catherine Desserts</a></td></tr><tr class="text-center"><td>14</td><td><a class="underline" href="https://https://ziahatchchileco.com" target="_blank">Zia Hatch Chile Company</a></td></tr><tr class="text-center"><td>15</td><td><a class="underline" href="https://https://zibakitchen.com" target="_blank">Ziba Kitchen</a></td></tr><tr class="text-center"><td>16</td><td><a class="underline" href="https://https://www.zoebakes.com" target="_blank">Zoë Bakes</a></td></tr><tr class="text-center"><td>17</td><td><a class="underline" href="https://https://zonacooks.com" target="_blank">Zona Cooks</a></td></tr><tr class="text-center"><td>18</td><td><a class="underline" href="https://https://zsuzsaisinthekitchen.blogspot.com/" target="_blank">Zsuzsa is in the kitchen</a></td></tr><tr class="text-center"><td>19</td><td><a class="underline" href="https://https://zumavalley.com/blogs/smoothies-bowls" target="_blank">Zuma Valley</a></td></tr><tr class="text-center"><td>20</td><td><a class="underline" href="https://https://zuranazrecipe.com/" target="_blank">Zuranaz</a></td></tr><tr class="text-center"><td>21</td><td><a class="underline" href="https://https://zweigles.com/recipes/" target="_blank">Zweigles</a></td></tr>"#);
+        let num_rows = res.text().split("<tr ").collect::<Vec<_>>().len();
+        assert_eq!(num_rows, Website::all().len() + 1);
+        res.assert_text_contains(r#"<tr class="text-center"><td>1</td><td><a class="underline" href="https://https://www.allrecipes.com/" target="_blank">Allrecipes</a></td></tr>"#);
         Ok(())
     }
 }
